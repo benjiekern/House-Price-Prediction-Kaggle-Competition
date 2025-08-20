be34d1fedaff7c585fd73c9a5fad751c4e6d803e
benji <benjiekern@gmail.com>
2025-08-20 20:09:16 +0200

    Added Pipeline

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBRegressor
 
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
@@ -8,14 +14,33 @@ train = train.loc[train_index]
 y_train = train['SalePrice']
 X_train = train.drop('SalePrice', axis=1)
 
-fill_values = {}
+numeric_cols = X_train.select_dtypes(include='number').columns
+categorical_cols = X_train.select_dtypes(include='object').columns
 
-for col in X_train.columns:
-    if X_train[col].dtype == 'object':
-        fill_values[col] = X_train[col].mode()[0]
-    else:
-        fill_values[col] = X_train[col].median()
+for col in numeric_cols:
+    lower_bound = X_train[col].quantile(0.01)
+    upper_bound = X_train[col].quantile(0.99)
+    X_train[col] = X_train[col].clip(lower=lower_bound, upper=upper_bound)
+    test[col] = test[col].clip(lower=lower_bound, upper=upper_bound)
 
-X_train = X_train.fillna(fill_values)
-X_test = test.fillna(fill_values)
+numerical_transformer = Pipeline([
+    ('imputer', SimpleImputer(strategy='median'))
+])
+
+categorical_transformer = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer([
+    ('num', numerical_transformer, numeric_cols),
+    ('cat', categorical_transformer, categorical_cols)
+])
+
+pipeline = Pipeline([
+    ('preprocessor', preprocessor),
+    ('classifier', XGBRegressor())
+])
+
+pipeline.fit(X_train, y_train)
 
